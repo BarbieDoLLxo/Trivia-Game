b93abdc6f0b97e7c905ff7e175372fa4fafdc50e
barbiedoll <Barbie.Slumerican@gmail.com>
2019-07-20 22:08:45 -0600

    updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,7 +18,7 @@ start();
 
 
 var clockRunning=false;
-var time=30;
+var time=15;
 var intervalId;
 
 
@@ -124,4 +124,491 @@ function timeConverter(t) {
 
   return minutes + ":" + seconds;
 }
+
+
+
+
+
+(function() {
+  function buildQuiz() {
+    // we'll need a place to store the HTML output
+    var output = [];
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // we'll want to store the list of answer choices
+      var answers = [];
+
+      // and for each available answer...
+      for (letter in currentQuestion.answers) {
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+
+      // add this question and its answers to the output
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join("")} </div>`
+      );
+    });
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join("");
+  }
+
+  function showResults() {
+    // gather answer containers from our quiz
+    var answerContainers = quizContainer.querySelectorAll(".answers");
+
+    // keep track of user's answers
+    var numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // find selected answer
+      var answerContainer = answerContainers[questionNumber];
+      var selector = "input[name=question ${questionNumber}]:checked";
+      var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if (userAnswer === currentQuestion.correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+        
+
+        // color the answers green
+        answerContainers[questionNumber].style.color = "lightgreen";
+      } else {
+        // if answer is wrong or blank
+        // color the answers red
+        answerContainers[questionNumber].style.color = "red";
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  
+
+
+  var quizContainer = document.getElementById("quiz");
+  var resultsContainer = document.getElementById("results");
+  var submitButton = document.getElementById("submit");
+  var myQuestions = [
+    {
+      question: "Which of these is a fruit?",
+      answers: {
+        a: "Avacado",
+        b: "Lettuce",
+        c: "Tomato"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "What was Waldo wearing?",
+      answers: {
+        a: "Blue Squares",
+        b: "Green Circles",
+        c: "Red Stripes"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Why is it so hot ouside?",
+      answers: {
+        a: "Global Warming",
+        b: "Too Much Exploring the Pacific Ocean",
+        c: "I don't Know",
+        d: "Hell is on Earth"
+      },
+      correctAnswer: "d"  
+    }
+  ];
+
+
+ 
+
+
+
+
+
+
+  // display quiz right away
+  buildQuiz();
+
+  // on submit, show results
+  submitButton.addEventListener("click", showResults);
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// quizContainer = document.getElementById('#quiz');
+//   resultsContainer = document.getElementById('#results');
+//  submitButton = document.getElementById('#submit');
+
+// // paste soultion
+
+// function buildQuiz(){}
+
+// function showResults(){}
+
+// // display quiz right away
+// buildQuiz();
+
+// // on submit, show results
+// submitButton.addEventListener('click', showResults);
+
+
+
+
+//  myQuestions = [
+//   {
+//     question: "Who is the strongest?",
+//     answers: {
+//       a: "Superman",
+//       b: "The Terminator",
+//       c: "Waluigi, obviously"
+//     },
+//     correctAnswer: "c"
+//   },
+//   {
+//     question: "What is the best site ever created?",
+//     answers: {
+//       a: "SitePoint",
+//       b: "Simple Steps Code",
+//       c: "Trick question; they're both the best"
+//     },
+//     correctAnswer: "c"
+//   },
+//   {
+//     question: "Where is Waldo really?",
+//     answers: {
+//       a: "Antarctica",
+//       b: "Exploring the Pacific Ocean",
+//       c: "Sitting in a tree",
+//       d: "Minding his own business, so stop asking"
+//     },
+//     correctAnswer: "d"
+//   }
+// ];
+
+// function buildQuiz(){
+//   // we'll need a place to store the HTML output
+//    output = [];
+
+//   // for each question...
+//   myQuestions.forEach(
+//     (currentQuestion, questionNumber) => {
+
+//       // we'll want to store the list of answer choices
+//        answers = [];
+
+//       // and for each available answer...
+//       for(letter in currentQuestion.answers){
+
+//         // ...add an HTML radio button
+//         answers.push(
+//           `<label>
+//             <input type="radio" name="question${questionNumber}" value="${letter}">
+//             ${letter} :
+//             ${currentQuestion.answers[letter]}
+//           </label>`
+//         );
+//       }
+
+//       // add this question and its answers to the output
+//       output.push(
+//         `<div class="question"> ${currentQuestion.question} </div>
+//         <div class="answers"> ${answers.join('')} </div>`
+//       );
+//     }
+//   );
+
+//   // finally combine our output list into one string of HTML and put it on the page
+//   quizContainer.innerHTML = output.join('');
+// }
+
+
+// myQuestions.forEach( (currentQuestion, questionNumber) => {
+//   // 
+  
+  
+  
+  
+//   // here goes the code we want to run for each question
+// });
+
+
+
+// // we'll want to store the list of answer choices
+// var answers = [];
+
+// // and for each available answer...
+// for(letter in currentQuestion.answers){
+
+//   // ...add an html radio button
+//   answers.push(
+//     `<label>
+//       <input type="radio" name="question${questionNumber}" value="${letter}">
+//       ${letter} :
+//       ${currentQuestion.answers[letter]}
+//     </label>`
+//   );
+// }
+
+// // add this question and its answers to the output
+// output.push(
+//   `<div class="question"> ${currentQuestion.question} </div>
+//   <div class="answers"> ${answers.join('')} </div>`
+// );
+
+
+
+// quizContainer.innerHTML = output.join('');
+
+
+
+
+// function showResults(){
+
+//   // gather answer containers from our quiz
+//   var answerContainers = quizContainer.querySelectorAll('.answers');
+
+//   // keep track of user's answers
+//   var numCorrect = 0;
+
+//   // for each question...
+//   myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+//     // find selected answer
+//     var answerContainer = answerContainers[questionNumber];
+//     var selector = 'input[name=question'+questionNumber+']:checked';
+//     var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+//     // if answer is correct
+//     if(userAnswer===currentQuestion.correctAnswer){
+//       // add to the number of correct answers
+//       numCorrect++;
+
+//       // color the answers green
+//       answerContainers[questionNumber].style.color = 'lightgreen';
+//     }
+//     // if answer is wrong or blank
+//     else{
+//       // color the answers red
+//       answerContainers[questionNumber].style.color = 'red';
+//     }
+//   });
+
+//   // show number of correct answers out of total
+//   resultsContainer.innerHTML = numCorrect + ' out of ' + myQuestions.length;
+// }
+
+
+
+
+
+// // gather answer containers from our quiz
+//  answerContainers = quizContainer.querySelectorAll('.answers');
+
+// // keep track of user's answers
+// var  numCorrect = 0;
+
+
+
+// // for each question...
+//  myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+//   // find selected answer
+//   var answerContainer = answerContainers[questionNumber];
+//   var selector = `input[name=question${questionNumber}]:checked`;
+//   var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+//   // if answer is correct
+//   if(userAnswer===currentQuestion.correctAnswer){
+//     // add to the number of correct answers
+//     numCorrect++;
+
+//     // color the answers green
+//     answerContainers[questionNumber].style.color = 'lightgreen';
+//   }
+//   // if answer is wrong or blank
+//   else{
+//     // color the answers red
+//     answerContainers[questionNumber].style.color = 'red';
+//   }
+// });
+
+
+
+// // find selected answer
+// var answerContainer = answerContainers[questionNumber];
+// var selector = `input[name=question${questionNumber}]:checked`;
+// var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+
+
+
+// // if answer is correct
+// if(userAnswer===currentQuestion.correctAnswer){
+//   // add to the number of correct answers
+//   numCorrect++;
+
+//   // color the answers green
+//   answerContainers[questionNumber].style.color = 'lightgreen';
+// }
+// // if answer is wrong or blank
+// else{
+//   // color the answers red
+//   answerContainers[questionNumber].style.color = 'red';
+// }
+
+
+// // show number of correct answers out of total
+// resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+
+
+
+// (function(){
+//   // put the rest of your code here
+// })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
